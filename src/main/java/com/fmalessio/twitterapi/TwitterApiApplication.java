e82bd225b06c426902b78bef9bee868fc96dd6c3
Fede Alessio <alessiofederico@hotmail.com>
2019-03-18 21:13:29 -0300

    Configuring App for Heroku 2

--- src/main/java/com/fmalessio/twitterapi/TwitterApiApplication.java
+++ src/main/java/com/fmalessio/twitterapi/TwitterApiApplication.java
@@ -1,18 +1,13 @@
 package com.fmalessio.twitterapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class })
 public class TwitterApiApplication extends SpringBootServletInitializer {
 
 	@Override
